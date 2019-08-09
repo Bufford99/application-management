ca8390f9deeaa0fec5c52e2cd9dc2fc81ea71a8c
Bufford Brian Sta Maria <iambufford99@gmail.com>
2019-08-09 14:48:14 -0400

    feat: table paginator

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { MatSort, MatTableDataSource } from '@angular/material';
+import { MatSort, MatTableDataSource, MatPaginator } from '@angular/material';
 
 import applicants from '../../assets/data/applications.json';
 
@@ -17,6 +17,7 @@ export class ListComponent implements OnInit {
   displayedColumns = ['id', 'name', 'position', 'applied', 'experience'];
 
   @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
   constructor(
     private router: Router
@@ -26,6 +27,7 @@ export class ListComponent implements OnInit {
     NavbarComponent.setControlHeader('Application Management');
 
     this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
   showApplicationDetails(id: string): void {
